6fa763f1627b10916683d6d1581fbbaf85ddd045
Eric Salas <esalas.0691@hotmail.com>
2016-02-21 20:29:41 +0000

    json

--- js/paises.js
+++ js/paises.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
     
     
-    var historiaId = window.location.search.substring(4);
+    var paisId = window.location.search.substring(4);
     
     $.ajax({
         method: 'get',
         url: 'http://ec2-52-10-12-157.us-west-2.compute.amazonaws.com:3500/pais',
-        data: {id: historiaId},
-        success: function(historia){
-            if(historia!=='0'){
-            $('#nombrePais').text(historia[0].cuidad);
-             $('#titulo1').text(historia[0].pais);
-              $('#bandera').addClass('flag-icon-'+historia[0].bandera);
-              $('#capaHistoria').css('background-image','url(img/'+historia[0].capa+'.jpg)');
-              $('#fecha').text(historia[0].fecha);
+        data: {id: paisId},
+        success: function(pais){
+            if(pais!=='0'){
+                console.log(pais[0].historias[0].id);
+            $('#nombrePais').text(pais[0].cuidad);
+             $('#titulo1').text(pais[0].pais);
+              $('#bandera').addClass('flag-icon-'+pais[0].bandera);
+              $('#capaHistoria').css('background-image','url(img/'+pais[0].capa+'.jpg)');
+              $('#fecha').text(pais[0].fecha);
             }
         },
         error: function(){
--- web_services/init.js
+++ web_services/init.js
@@ -1,11 +1,11 @@
-
-
 var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extendedd:true}));
+app.use(bodyParser.urlencoded({
+    extendedd: true
+}));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -14,57 +14,97 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get('/historia',function(req,res){
-var id = req.query.id;
-switch(id){
-	case '1':
-		var historia = [{"capa":"sp2","bandera":"br","cuidad":"Sao Paulo, Brasil","titulo1":"Como es vivir en Sao Paulo,","titulo2":"la mayor cuidad de America Latina.","fecha":"06 de marzo, 2016."}];
-		res.contentType('application/json');
-		res.send(JSON.stringify(historia));
-		break;
-     case '2':
-                 var historia = [{"capa":"rio2","bandera":"br","cuidad":"Rio de Janeiro, Brasil","titulo1":"¿Qué hacer? ¿Qué conocer? en Río de Janeiro.","titulo2":"La Cidade Maravilhosa.","fecha":" 6 de marzo, 2016."}];
-                res.contentType('application/json');
-                res.send(JSON.stringify(historia));
-                break;
+app.get('/historia', function(req, res) {
+    var id = req.query.id;
+    switch (id) {
+        case '1':
+            var historia = [{
+                "capa": "sp2",
+                "bandera": "br",
+                "cuidad": "Sao Paulo, Brasil",
+                "titulo1": "Como es vivir en Sao Paulo,",
+                "titulo2": "la mayor cuidad de America Latina.",
+                "fecha": "06 de marzo, 2016."
+            }];
+            res.contentType('application/json');
+            res.send(JSON.stringify(historia));
+            break;
+        case '2':
+            var historia = [{
+                "capa": "rio2",
+                "bandera": "br",
+                "cuidad": "Rio de Janeiro, Brasil",
+                "titulo1": "¿Qué hacer? ¿Qué conocer? en Río de Janeiro.",
+                "titulo2": "La Cidade Maravilhosa.",
+                "fecha": " 6 de marzo, 2016."
+            }];
+            res.contentType('application/json');
+            res.send(JSON.stringify(historia));
+            break;
 
-         case '3':
-                 var historia = [{"capa":"santos2","bandera":"br","cuidad":"Santos, Brasil","titulo1":"Santos,","titulo2":"la cuidad del rey Pelé y Neymar Jr.","fecha":"6 de marzo, 2016."}];
-                res.contentType('application/json');
-                res.send(JSON.stringify(historia));
-                break;
-	default:
-	console.log('llegue');
-		res.send('0');
-}
+        case '3':
+            var historia = [{
+                "capa": "santos2",
+                "bandera": "br",
+                "cuidad": "Santos, Brasil",
+                "titulo1": "Santos,",
+                "titulo2": "la cuidad del rey Pelé y Neymar Jr.",
+                "fecha": "6 de marzo, 2016."
+            }];
+            res.contentType('application/json');
+            res.send(JSON.stringify(historia));
+            break;
+        default:
+            console.log('llegue');
+            res.send('0');
+    }
 });
 
 
-app.get('/pais',function(req,res){
-var id = req.query.id;
-switch(id){
-	case '1':
-		var historia = [{"capa":"argentina1","bandera":"ar","pais":"Argentina","cuidad":"Ushuaia, fin del mundo. Argentina "}];
-		res.contentType('application/json');
-		res.send(JSON.stringify(historia));
-		break;
-     case '2':
-                 var historia = [{"capa":"bolivia1","bandera":"bo","cuidad":"Copacabana, Bolivia"}];
-                res.contentType('application/json');
-                res.send(JSON.stringify(historia));
-                break;
+app.get('/pais', function(req, res) {
+    var id = req.query.id;
+    switch (id) {
+        case '1':
+            var pais = [{
+                "capa": "argentina1",
+                "bandera": "ar",
+                "pais": "Argentina",
+                "cuidad": "Ushuaia, fin del mundo. Argentina "
+            }];
+            res.contentType('application/json');
+            res.send(JSON.stringify(historia));
+            break;
+        case '2':
+            var pais = [{
+                "capa": "bolivia1",
+                "bandera": "bo",
+                "cuidad": "Copacabana, Bolivia"
+            }];
+            res.contentType('application/json');
+            res.send(JSON.stringify(historia));
+            break;
 
-         case '3':
-                 var historia = [{"capa":"fernando-noronha","bandera":"br","cuidad":"Fernando Noronha, Brasil","pais":"Brasil"}];
-                res.contentType('application/json');
-                res.send(JSON.stringify(historia));
-                break;
-	default:
-	console.log('llegue');
-		res.send('0');
-}
+        case '3':
+            var pais = [{
+                "capa": "fernando-noronha",
+                "bandera": "br",
+                "cuidad": "Fernando Noronha, Brasil",
+                "pais": "Brasil",
+                "historias": [{
+                    "id": "1",
+                    "titulo": "Como es vivir en São Paulo, la mayor cuidad de América Latina.",
+                    "foto": "sp1"
+                }]
+            }];
+            res.contentType('application/json');
+            res.send(JSON.stringify(historia));
+            break;
+        default:
+            console.log('llegue');
+            res.send('0');
+    }
 });
 
-app.listen(3500, function(){
-console.log('Running');
+app.listen(3500, function() {
+    console.log('Running');
 });
