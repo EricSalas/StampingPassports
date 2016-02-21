d9bd09ca72191ae1e3a132e68b54e79929836cdc
Eric Salas <esalas.0691@hotmail.com>
2016-02-21 20:45:58 +0000

    json + historias

--- js/paises.js
+++ js/paises.js
@@ -9,12 +9,26 @@ $(document).ready(function() {
         data: {id: paisId},
         success: function(pais){
             if(pais!=='0'){
-                console.log(pais[0].historias[0].titulo);
             $('#nombrePais').text(pais[0].cuidad);
              $('#titulo1').text(pais[0].pais);
               $('#bandera').addClass('flag-icon-'+pais[0].bandera);
               $('#capaHistoria').css('background-image','url(img/'+pais[0].capa+'.jpg)');
-              $('#fecha').text(pais[0].fecha);
+              var indice = 0;
+              var html = "";
+              $.each(pais[0].historias, function(i,h){
+                  html+= '<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">'+
+'<div class="panel panel-default">'+
+'<div class="panel-heading">'+
+'<h3 class="panel-title">'+h.titulo+'</h3>'+
+'</div>'+
+'<div class="panel-body">'+
+'<img  class="featurette-image img-responsive center-block" src="img/'+h.foto+'.jpg" alt="Generic placeholder image">'+
+'</div>'+
+'<div class="panel-footer">Leer historia</div>'+
+'</div>'+
+'</div>'});
+
+$('.historias').html('').html(html);
             }
         },
         error: function(){
